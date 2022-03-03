c123f2f405a141c2f0027e4168e26ff87b677ee1
OphirNaor <ophir1991@gmail.com>
2022-03-03 08:48:05 +0200

    first styling

--- js/apps/mail/pages/mail-app.cmp.js
+++ js/apps/mail/pages/mail-app.cmp.js
@@ -1,26 +1,26 @@
 import mailList from "../components/mail-list.cmp.js"
-import {mailService} from "../services/mail-service.js"
+import { mailService } from "../services/mail-service.js"
 
 export default {
     template: `
-    <section class="mail-app">
+    <section class="mail-app app-main">
         <h1>Email:</h1>
        
      <mail-list :mails="mailsToShow"> </mail-list> 
     </section>
     `,
 
-components: {
-    mailList,
-},
+    components: {
+        mailList,
+    },
 
     created() {
         mailService.query()
-        .then(mails => this.mails = mails);
+            .then(mails => this.mails = mails);
     },
     data() {
         return {
-            mails:null,
+            mails: null,
 
 
         }
@@ -29,9 +29,10 @@ components: {
     computed: {
         mailsToShow() {
             return this.mails;
-           
+
         }
-    }};
+    }
+};
 
      // if (!this.filterBy) return this.mails;
             // const regex = new RegExp(this.filterBy.subject, 'i');
--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -4,17 +4,17 @@ export default {
     <header class="app-header">
         <nav>
             <section>
-                <router-link to="/" active-class="active-link"><i class="fa-solid fa-house"></i>
+                <router-link to="/" active-class="active-link">
                     <div class="logo">
                         <h3>Apsus</h3>
-                        <img src="img/logo.png">
+                        <i class="fa-solid fa-house"></i>
                     </div>
                 </router-link>
             </section>
             <section class = "nav-links">
                 <router-link active-class="active" class="header-link" to="/mail">Mail </router-link>|
                 <router-link class="header-link" to="/note">Keep</router-link>|
-                <!-- <router-link class="header-link" to="/book">Books</router-link> -->
+                <router-link class="header-link" to="/book">Books</router-link>
             </section>
         </nav>
     </header>
