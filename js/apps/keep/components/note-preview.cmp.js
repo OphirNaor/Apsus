e1b2da266f91d06ba5de9c5e6800f47c691e88b3
OphirNaor <ophir1991@gmail.com>
2022-03-03 09:40:33 +0200

    editing note preview

--- js/apps/keep/components/note-preview.cmp.js
+++ js/apps/keep/components/note-preview.cmp.js
@@ -8,12 +8,49 @@ export default {
     template: `
     <section class="note-priview">
         <h4>{{note.info.title}}</h4>
+        <section class="pin-container"><a class="fa-solid fa-thumbtack pin-note" @click.stop="pin(note.id)"title="Pin note">
+        </a></section>
+        <div class="actions-container">
+        <a class="fa fa-trash" @click.stop="remove(note.id)"title="Delete note"></a>
+        <a class="fa fa-clone duplicate-note" @click.stop="duplicate(note.id)"title="Duplicate note"></a>
+        <a class="fa fa-envelope" @click.stop="sendByMail"></a>
+        <a class="" @click="">&#10530</a>
+        <a class = "fa-solid fa-palette color-palette-container" :class="{'show-colors':showColors}"  @click.stop="showColors=!showColors" title="Pick color" >
+            <ul class = "color-palette">
+                <li v-for="color in colors" @click.stop ="setBgc(note.id,color),showColors=!showColors" :style = "{'background-color':color}"></li>
+            </ul>
+        </a>
+    </div>
+    </section>
+    
+    
+    `,
+    template: `
+    <section class="note-priview">
+        <h4>{{note.info.title}}</h4>
+        <div class="actions-container">
+            <section class="pin-container"><a class="fa-solid fa-thumbtack pin-note" @click.stop="pin(note.id)"title="Pin note">
+                </a></section>
+            <a class="fa fa-trash" @click.stop="remove(note.id)"title="Delete note"></a>
+            <a class="fa fa-clone duplicate-note" @click.stop="duplicate(note.id)"title="Duplicate note"></a>
+            <a class="fa fa-envelope" @click.stop="sendByMail"></a>
+            <a class="" @click="">&#10530</a>
+            <a class = "fa-solid fa-palette color-palette-container" :class="{'show-colors':showColors}"  @click.stop="showColors=!showColors" title="Pick color" >
+                <ul class = "color-palette">
+                    <li v-for="color in colors" @click.stop ="setBgc(note.id,color),showColors=!showColors" :style = "{'background-color':color}"></li>
+                </ul>
+            </a>
+    </div>
     </section>
     
     
     `,
     data() {
         return {
+            bgc: this.note.style.bgc,
+            colors: ['#f28b82', '#fbbc04', '#fff475', '#ccff90', '#a7ffeb', '#cbf0f8', '#aecbfa', '#d7aefb', '#fdcfe8', '#e6c9a8', '#e8eaed', '#ffffff'],
+            showColors: false,
+            selected: false
 
 
         };
@@ -22,6 +59,19 @@ export default {
 
     },
     methods: {
+        pin(noteId) {
+            eventBus.emit('pinnedNote', noteId)
+        },
+        remove(noteId) {
+            eventBus.emit('removedNote', noteId)
+        },
+        setBgc(noteId, color) {
+            eventBus.emit('setBgc', noteId, color)
+        },
+        duplicate(noteId) {
+            eventBus.emit('duplicateNote', noteId)
+        },
+
 
 
     },
